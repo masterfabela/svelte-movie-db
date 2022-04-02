4982622077c5a3a9d52db1add4cc1ae3cbf2b66a
Jose Ramon Vieites <jose.vieites@altia.es>
2022-04-02 22:38:33 +0200

    Bases svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let count = 0;
+
+	function addOne() {
+		count++;
+	}
+</script>
+
+<h1>Number: {count}</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<a href="/about">Go to about</a>
+<button on:click={addOne}>Increment</button>
+
+<style>
+	p {
+		font-size: 2rem;
+	}
+</style>
